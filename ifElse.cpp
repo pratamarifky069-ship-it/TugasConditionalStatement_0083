0e7212482f4f0746c8406b3ec7642d279baded23
RifkyIkhsan <pratamarifky069-ship-it>
2026-03-11 05:25:21 +0700

    init program BMI

---
+++ ifElse.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+//Prosedur untuk input data
+void inputData(float &berat, float &tinggi){
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
+
+//Fungsi menghitung BMI
+float hitungBMI(float berat, float tinggi){
+    return berat / (tinggi * tinggi);
+}
+
+//Fungsi menentukan status BMI
+string statusBMI(float bmi){
+    if(bmi < 18.5)
+        return "Berat Badan Kurang";
+    else if(bmi < 25)
+        return "Berat Badan Normal";
+    else if(bmi < 30)
+        return "Berat Badan Kelebihan";
+    else 
+        return "Obesitas";
+}
+int main(){
+    float berat, tinggi, bmi;
+    string status;
+    cout << "--- Selamat Datang Mahasiswa Kelas B 2025 ---" <<endl;
+
+    inputData(berat, tinggi);
+    cout <<endl << "--- Hasil ---" <<endl;
+
+    bmi = hitungBMI(berat, tinggi);
+    status = statusBMI(bmi);
+    
+    cout << "BMI Anda : " <<bmi <<endl;
+    cout << "Status   : " <<status <<endl;
+    return 0;
+}
